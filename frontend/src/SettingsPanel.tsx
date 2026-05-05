74447761ddb090477f39992ca0624a2e99f0d743
Heaton <newwaylau@hotmail.com>
2026-05-05 21:43:55 +0800

    fix: 快捷键文字减短

--- frontend/src/SettingsPanel.tsx
+++ frontend/src/SettingsPanel.tsx
@@ -232,7 +232,7 @@ export default function SettingsPanel({ open, onClose, mode, onModeChange, voice
         <div className="settings-section">
           <label>快捷键</label>
           <div style={{ fontSize: '0.8rem', color: 'var(--text-muted)', lineHeight: 2 }}>
-            <kbd>-</kbd> 剧集原音 &nbsp; <kbd>=</kbd> 选中的音色(默认 Ryan) &nbsp; <kbd>\</kbd> 下一句<br />
+            <kbd>-</kbd> 剧集原音 &nbsp; <kbd>=</kbd> 音色(默认 Ryan) &nbsp; <kbd>\</kbd> 下一句<br />
             <kbd>[</kbd> 中文 &nbsp; <kbd>]</kbd> 答案 &nbsp; <kbd>Enter</kbd> 提交
           </div>
         </div>
